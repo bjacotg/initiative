5c2c50be1fd3cff4cef80b88cd8b4abca0e9de55
bastienjacot-guillarmod <bastienjacot-guillarmod@v1426-wn-172-21-104-127.campus-dynamic.uwaterloo.ca>
2013-04-01 17:33:16 -0400

    Db definition online

--- Initiative/db_init.sql
+++ Initiative/db_init.sql
@@ -7,11 +7,41 @@ SET storage_engine = INNODB;
 --DELETE FROM mysql.user WHERE user LIKE 'ECEG2-%';
 --DELETE FROM mysql.db WHERE user LIKE 'ECEG2-%';
 --FLUSH PRIVILEGES;
-
+DROP TABLE IF EXISTS Vote;
+DROP TABLE IF EXISTS Initiative;
 DROP TABLE IF EXISTS Voter;
+DROP TABLE IF EXISTS Comitee;
 
 CREATE TABLE Voter(
-	HashId char(40),
-	Password varchar(10));
+	Id char(40),
+	Password char(128),
+        Salt char(64),
+        CONSTRAINT PKVoter PRIMARY KEY(Id)
+);
+
+CREATE TABLE Comitee(
+	Id char(40),
+	Password char(128),
+        Salt char(64),
+        CONSTRAINT PKComitee PRIMARY KEY(Id)
+);
+
+
+CREATE TABLE Initiative(
+    InitID integer AUTO_INCREMENT,
+    ComiteeId char(40),
+    CONSTRAINT PKInit PRIMARY KEY(InitID),
+    CONSTRAINT FKInitCom FOREIGN KEY(ComiteeId) REFERENCES Comitee (Id)
+);
+
+CREATE TABLE Vote(
+    Initiative integer,
+    Id char(40),
+    CONSTRAINT PKVote PRIMARY KEY(Initiative, Id),
+    CONSTRAINT FKVoteVoter FOREIGN KEY(Id) REFERENCES Voter (Id),
+    CONSTRAINT FKVoteInit FOREIGN KEY(Initiative) REFERENCES Initiative (InitID)
+);
+
+
 
 SET AUTOCOMMIT = 0;
